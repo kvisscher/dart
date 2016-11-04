82427b13e83ff7a237a7a248bf648d91cc8bec82
Djowie <Joey@joeys-mbp.home>
2016-11-05 00:26:32 +0100

    fixes dart

--- web/main.dart
+++ web/main.dart
@@ -1,13 +1,13 @@
 import 'dart:html';
+import 'dart:html';
+import 'dart:convert';
 
-void main() {
 // Copyright (c) 2015, the Dart project authors.
 // Please see the AUTHORS file for details.
 // All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:html';
-import 'dart:convert';
+
 
 // Input fields
 InputElement favoriteNumber;
@@ -121,4 +121,3 @@ void showJson(Event e) {
   mapAsJson.text    = JSON.encode(formData);
 }
 
-}
